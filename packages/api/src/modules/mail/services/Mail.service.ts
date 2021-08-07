7a2f6ad5b0220d3b744365663c1fcfbca479fd88
Juiise Nexit <soglacen@gmail.com>
2021-08-07 21:43:20 +0300

    Made nodemailer use secure protocol

--- packages/api/src/modules/mail/services/Mail.service.ts
+++ packages/api/src/modules/mail/services/Mail.service.ts
@@ -6,7 +6,7 @@ export class MailService {
   private client = mailer.createTransport({
     host: String(process.env.MAIL_HOST),
     port: Number(process.env.MAIL_PORT),
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.MAIL_AUTH_USER,
       pass: process.env.MAIL_AUTH_PASS,
