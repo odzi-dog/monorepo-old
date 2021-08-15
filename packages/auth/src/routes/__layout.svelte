422146eb8501cda342af4e76b7a1aad38ac1a8ae
Juiise Nexit <soglacen@gmail.com>
2021-08-15 11:39:32 +0300

    auth: Fix CustomDesign's backgroundColor property

--- packages/auth/src/routes/__layout.svelte
+++ packages/auth/src/routes/__layout.svelte
@@ -5,36 +5,47 @@
   import { page } from '$app/stores';
 
   import { DesignStore } from '../stores/design/Design.store'; 
+  let loading = true;
 
   onMount(() => {
     if ($page.query.get('design') != null) {
       DesignStore.setDesign($page.query.get('design'));
+
+      console.log($DesignStore.custom);
     };
+    
+    loading = false;
   });
 </script>
 
-<!-- Layout -->
-<main style="background-color: { $DesignStore?.custom?.backgroundColor || "rgba(229, 231, 235);" }" class="w-full h-screen flex flex-col items-center justify-between py-6 md:py-16">
-  <!-- Container -->
-  <div class="w-full px-12 md:px-0 md:w-1/3 h-full flex flex-col bg-white rounded-xl">
-    <!-- Header -->
-    <div class="w-full py-6 hidden md:flex items-center justify-center">
-      <img class="w-14 h-14 md:w-10 md:h-10" src="{ $DesignStore?.custom?.logotype || "/logotypes/odzi-dog-small-black.svg" }" alt="">
-    </div>
-    
-    <!-- Content -->
-    <div class="flex-grow relative px-4 md:pb-10">
-      <slot></slot>
-    </div>
-  </div>
+<svelte:head>
+  <title>Авторизация | auth.odzi.dog</title>
+</svelte:head>
 
-  <!-- Footer -->
-  <div class="hidden w-full absolute inset-x-0 bottom-0 py-6 md:flex flex-col items-center justify-center">
-    <!-- Links -->
-    <div class="flex items-center opacity-60">
-      <Icon name='shield' attrs={{ class: 'w-4 h-4 text-white mr-1' }} />
-
-      <p class="text-sm text-white">Защищенно <span class="underline">auth.odzi.dog</span></p>
+{ #if !loading }
+  <!-- Layout -->
+  <main style="background-color: { $DesignStore?.custom?.backgroundColor || "rgba(229, 231, 235);" }" class="w-full h-screen flex flex-col items-center justify-between py-6 md:py-16">
+    <!-- Container -->
+    <div class="w-full px-12 md:px-0 md:w-1/3 h-full flex flex-col bg-white rounded-xl">
+      <!-- Header -->
+      <div class="w-full py-6 hidden md:flex items-center justify-center">
+        <img class="w-14 h-14 md:w-10 md:h-10" src="{ $DesignStore?.custom?.logotype || "/logotypes/odzi-dog-small-black.svg" }" alt="">
+      </div>
+      
+      <!-- Content -->
+      <div class="flex-grow relative px-4 md:pb-10">
+        <slot></slot>
+      </div>
     </div>
-  </div>
-</main>
+
+    <!-- Footer -->
+    <div class="hidden w-full absolute inset-x-0 bottom-0 py-6 md:flex flex-col items-center justify-center">
+      <!-- Links -->
+      <div class="flex items-center opacity-60">
+        <Icon name='shield' attrs={{ class: 'w-4 h-4 text-white mr-1' }} />
+
+        <p class="text-sm text-white">Защищенно <span class="underline">auth.odzi.dog</span></p>
+      </div>
+    </div>
+  </main>
+{ /if }
---
+++ packages/auth/src/routes/test.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+
+  onMount(() => {
+    let json = {
+      backgroundColor: "#111827",
+      logotype: "https://res.cloudinary.com/lococovu-cdn/image/upload/v1614110162/logotypes/pawcapsu-black-small.svg"
+    };
+
+    goto(`/callback/${encodeURIComponent('pawcapsu.ml/login')}?design=${encodeURIComponent(JSON.stringify(json))}`);
+  });
+</script>
