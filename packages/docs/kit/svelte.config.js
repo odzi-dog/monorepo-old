861cbd3da9005ddd7c9839afd55286ea6dcb6c7e
Juiise Nexit <soglacen@gmail.com>
2021-09-21 14:44:48 +0300

    docs: Create basic docs service (8/-)

--- packages/docs/kit/svelte.config.js
+++ packages/docs/kit/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '../adapter';
+import adapter from '../adapter/index.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
