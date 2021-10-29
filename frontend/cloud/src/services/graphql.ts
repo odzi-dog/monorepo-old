e6b58f7df2d96439a1dc134b9be2b445f510a8b2
Juiise Nexit <soglacen@gmail.com>
2021-10-29 14:22:06 +0300

    web/cloud: Text pipeline (2/-)

--- frontend/cloud/src/services/graphql.ts
+++ frontend/cloud/src/services/graphql.ts
@@ -1,10 +1,8 @@
-import { InMemoryCache } from '@apollo/client/core/core.cjs.js';
+import { InMemoryCache, gql } from '@apollo/client/core/core.cjs.js';
 import { SvelteApolloClient } from "svelte-apollo-client";
-import { gql } from '@apollo/client/core';
 
 import { Config } from '$config/index';
 
-// export { gql } from '@apollo/client/core/core.cjs.js';
 export const client = SvelteApolloClient({
   uri: String(Config.get("GRAPHQL_URL")),
   credentials: 'include',
